7d02f845b74c01a0d0a12cbb884e371960b2b1bc
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2022-10-28 16:32:06 +0000

    Initial commit

---
+++ Tests/AppTests/AppTests.swift
@@ -0,0 +1,20 @@
+import XCTest
+import class Foundation.Bundle
+
+final class AppTests: XCTestCase {
+    func testExample() throws {
+        XCTAssertTrue(true)
+    }
+
+    /// Returns path to the built products directory.
+    var productsDirectory: URL {
+      #if os(macOS)
+        for bundle in Bundle.allBundles where bundle.bundlePath.hasSuffix(".xctest") {
+            return bundle.bundleURL.deletingLastPathComponent()
+        }
+        fatalError("couldn't find the products directory")
+      #else
+        return Bundle.main.bundleURL
+      #endif
+    }
+}
